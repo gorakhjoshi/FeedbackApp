a9013117dff75025274a2327973dfd0b1a09e56e
Gorakh Raj Joshi <gorakh626@gmail.com>
2021-12-17 19:25:47 +0545

    dummy data generated

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+  {
+    id: 1,
+    rating: 8,
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos aliquid dolores dolor obcaecati totam! Maiores cumque beatae vero minus perferendis?',
+  },
+  {
+    id: 2,
+    rating: 5,
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos aliquid dolores dolor obcaecati totam! Maiores cumque beatae vero minus perferendis?',
+  },
+  {
+    id: 3,
+    rating: 10,
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos aliquid dolores dolor obcaecati totam! Maiores cumque beatae vero minus perferendis?',
+  },
+];
+
+export default FeedbackData;
