e6ad8d2c4bf0fcd3e61bd4ec6291f148685f95ec
Gorakh Raj Joshi <gorakh626@gmail.com>
2021-12-21 19:32:16 +0545

    close button icon added

--- src/components/FeedbackItem.js
+++ src/components/FeedbackItem.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Card from './shared/Card';
+import { FaTimesCircle } from 'react-icons/fa';
 
 function FeedbackItem({ item, handleDelete }) {
   return (
     <Card>
       <div className='num-display'>{item.rating}</div>
       <div className='text-display'>{item.text}</div>
-      <button onClick={() => handleDelete(item.id)}>Delete</button>
+      <button onClick={() => handleDelete(item.id)} className='close'>
+        <FaTimesCircle color='purple' size='2rem' />
+      </button>
     </Card>
   );
 }
