35d2070760aa6d3befca2828def8826434f007a3
Gorakh Raj Joshi <gorakh626@gmail.com>
2021-12-23 19:52:34 +0545

    handler function and state added in FeedbackForm Component

--- src/components/FeedbackForm.js
+++ src/components/FeedbackForm.js
@@ -3,12 +3,21 @@ import Button from './shared/Button';
 import Card from './shared/Card';
 
 function FeedbackForm() {
+  const [text, setText] = React.useState('');
+  const handleChangeText = (e) => {
+    setText(e.target.value);
+  };
+
   return (
     <Card>
       <form>
         <h2>Please type your feedback below.</h2>
         <div className='input-group'>
-          <input type='text' />
+          <input
+            type='text'
+            onChange={handleChangeText}
+            placeholder='Write your review'
+          />
           <Button> Send </Button>
         </div>
       </form>
